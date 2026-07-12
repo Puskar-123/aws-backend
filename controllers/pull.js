8e0bb18e364591a7086624a441d25d1ede523a60
Puskar porel <puskarporel6@gmail.com>
2026-07-12 11:31:34 +0530

    Synchronize pull with MongoDB

--- controllers/pull.js
+++ controllers/pull.js
@@ -1,37 +1,75 @@
 const fs = require("fs").promises;
 const path = require("path");
 const { s3, S3_BUCKET } = require("../config/aws-config");
+const Repository = require("../models/repoModel");
 
 async function pullRepo(req, res) {
-  try {
+  const { id } = req.params;
 
+  try {
+    // Find repository
+    const repo = await Repository.findById(id);
+
+    if (!repo) {
+      return res.status(404).json({
+        error: "Repository not found",
+      });
+    }
+
+    // Local .myGit folder
     const repoPath = path.resolve(process.cwd(), ".myGit");
 
-    const data = await s3.listObjectsV2({
-      Bucket: S3_BUCKET,
-      Prefix: "commits/",
-    }).promise();
+    // Get all objects from S3
+    const data = await s3
+      .listObjectsV2({
+        Bucket: S3_BUCKET,
+        Prefix: "commits/",
+      })
+      .promise();
 
+    const latestFiles = new Map();
+
+    // Download every object
     for (const object of data.Contents) {
-
       const key = object.Key;
 
       const destination = path.join(repoPath, key);
 
+      // Create folder if it doesn't exist
       await fs.mkdir(path.dirname(destination), {
         recursive: true,
       });
 
-      const file = await s3.getObject({
-        Bucket: S3_BUCKET,
-        Key: key,
-      }).promise();
+      // Download file
+      const file = await s3
+        .getObject({
+          Bucket: S3_BUCKET,
+          Key: key,
+        })
+        .promise();
 
+      // Save locally
       await fs.writeFile(destination, file.Body);
+
+      // Save latest file list (ignore commit.json)
+      const filename = path.basename(key);
+
+      if (filename !== "commit.json") {
+        latestFiles.set(filename, {
+          filename,
+          path: key,
+        });
+      }
     }
+
+    // Update MongoDB repository content
+    repo.content = [...latestFiles.values()];
+
+    await repo.save();
 
     return res.json({
       message: "Pull successful!",
+      files: repo.content,
     });
 
   } catch (err) {
